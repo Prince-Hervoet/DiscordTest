6eaf16001ab0ca998d87212b2e24cf3ed93d2632
Prince-Hervoet <122962161+Prince-Hervoet@users.noreply.github.com>
2023-04-24 02:59:09 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export const DiscordInfo = {
   TOKEN:
-    "MTA5ODYzNjY1OTA3NzE1Njk5Ng.GLmsBt.PYP51Al7CCR7v6hlJOGz3tt_otVX52FXyyWPsw",
-  APPID: "1098636659077156996",
+    "xxx",
+  APPID: "xxx",
   SECRET:
-    "jn2$^&(&$%*#^28yroun$&^@*&$*hfo2h3rjb8uvu!@#$jkh9@wfskdhf$$9gg7u89h3rjkbnfjnwefj",
+    "xxx",
 };
 
 export const FabrieUrls = {
-  CN: "https://fabrie.cn/discord/auth",
-  COM: "https://fabrie.com/discord/auth",
+  CN: "xxx",
+  COM: "xxx",
 };
